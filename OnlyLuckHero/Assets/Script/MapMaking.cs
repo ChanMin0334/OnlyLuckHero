16ebaffc9f701c422606ffca3ad273a4d9ba69a0
chanmin0334 <chanmin0334@naver.com>
2021-03-04 16:11:57 +0900

    [Create]MapMaking Skript
    맵 생성 알고리즘 초안 업데이트

---
+++ OnlyLuckHero/Assets/Script/MapMaking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMaking : MonoBehaviour
+{   
+    public int[,] map = new int[100, 100];
+    static void Main(string[] args)  //지금은 Main메소드이지만 나중에는 다른 메소드로 바꾼후 다른 클래스에서 호출하는 방식으로 할 것
+    {
+        int size = 4;
+        int way = 5;
+        int mapevent = 2;
+        int mapshop = 1;
+
+        MapMaking mapmaking = new MapMaking();
+        mapmaking.mapmake(size, way, mapevent, mapshop); //맵메이킹 테스트 나중에는 맵크기와 모든것을 배열로 관리후 각 스테이지에 맞게 배분
+    }
+    public void mapmake(int size,int way,int mapevent,int mapshop)  //가로세로의 길이, 길 개수, 이벤트등을 인자로 받는다.
+    {
+
+    }
+  
+}
