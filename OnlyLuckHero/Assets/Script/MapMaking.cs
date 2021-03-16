b42a4b8d553581fecf41d64b7cbbc40264abac02
chanmin0334 <chanmin0334@naver.com>
2021-03-16 11:57:18 +0900

    [Create]Mapmake
    맵 생성기능 추가

--- OnlyLuckHero/Assets/Script/MapMaking.cs
+++ OnlyLuckHero/Assets/Script/MapMaking.cs
@@ -3,51 +3,222 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapMaking : MonoBehaviour
-{   
-    public int[,] map = new int[100, 100];
-    void awake()  //지금은 Main메소드이지만 나중에는 다른 메소드로 바꾼후 다른 클래스에서 호출하는 방식으로 할 것
+{
+    int stage = 0;
+    public void Awake()
     {
-        int size = 4;
-        int way = 5;
-        int mapevent = 2;
-        int mapshop = 1;
+        List<List<int>> Map = new List<List<int>>();
+        int[] size = { 5, 50, 6, 7 };
+        int[] way = { 6, 200, 9, 12 };
+        int[] mapevent = { 3, 30, 5, 6 };
+        int[] mapshop = { 3, 50, 1, 2 };
 
-        MapMaking mapmaking = new MapMaking();
-        mapmaking.mapmake(size, way, mapevent, mapshop); //맵메이킹 테스트 나중에는 맵크기와 모든것을 배열로 관리후 각 스테이지에 맞게 배분
+        int startx = Random.Range(0, size[stage] - 1);
+        int starty = Random.Range(0, size[stage] - 1);
+
+       makebasicmap(size[stage], way[stage], mapevent[stage], mapshop[stage], startx, starty, Map);
     }
-    public void mapmake(int size,int way,int mapevent,int mapshop)  //가로세로의 길이, 길 개수, 이벤트등을 인자로 받는다.
+
+    public void makebasicmap(int size, int way, int mapevent, int mapshop, int startx, int starty, List<List<int>> Map) //비어있는 맵 만들기
     {
-        for(int i = 0; i <= size; i++)
+        for (int i = 0; i < size; i++)
         {
-            for(int j = 0; j <= size; j++)
+            Map.Add(new List<int>());
+            for (int j = 0; j < size; j++)
             {
-                if (i == size || j == size)
+                Map[i].Add(0);
+            }
+        }
+        mapmake(size, way, mapevent, mapshop, startx, starty, Map);
+    }
+    public void mapmake(int size, int way, int mapevent, int mapshop, int startx, int starty, List<List<int>> Map)  //가로세로의 길이, 길 개수, 이벤트등을 인자로 받는다.
+    {
+        int maptile = 0;
+        bool isdirsel = false; //방향선택확인
+        bool istilesel = false;
+        int dir;
+
+        while (!istilesel)
+        {
+            switch (Random.Range(1, 4))
+            {
+                case 1:
+                    if (way - 1 >= 0) //남아있는 way 가 있다면
+                    {
+                        maptile = 1;
+                        way--;
+                        istilesel = true;
+                    }
+
+                    break;
+
+                case 2:
+                    if (mapevent - 1 >= 0) //남아있는 mapevent 가 있다면
+                    {
+                        maptile = 2;
+                        mapevent--;
+                        istilesel = true;
+                    }
+
+                    break;
+                case 3:
+                    if (mapshop - 1 >= 0) //남아있는 mapshop 가 있다면
+                    {
+                        maptile = 3;
+                        mapshop--;
+                        istilesel = true;
+                    }
+
+                    break;
+            }
+        }
+        int trynum = 0;
+
+        while (true)
+        {
+            if (trynum > 20)
+            {
+                resetpoint(size, way, mapevent, mapshop, startx, starty, Map);
+            }
+            try
+            {
+                dir = Random.Range(1, 5); //1234가 나옴 상하좌우 순서
+                switch (dir)
                 {
-                    map[i, j] = -2; //테두리 치기
+                    case 1:
+                        if (Map[startx + 1][starty] != 0)
+                        {
+                            trynum++;
+                            break;
+                        }
+                        else
+                        {
+                            Map[startx + 1][starty] = maptile;
+                            startx++;
+                            isdirsel = true;
+                            break;
+                        }
+                    case 2:
+                        if (Map[startx - 1][starty] != 0)
+                        {
+                            trynum++;
+                            break;
+                        }
+                        else
+                        {
+                            Map[startx - 1][starty] = maptile;
+                            startx--;
+                            isdirsel = true;
+                            break;
+                        }
+                    case 3:
+                        if (Map[startx][starty - 1] != 0)
+                        {
+                            trynum++;
+                            break;
+                        }
+                        else
+                        {
+                            Map[startx][starty - 1] = maptile;
+                            starty--;
+                            isdirsel = true;
+                            break;
+                        }
+                    case 4:
+                        if (Map[startx][starty + 1] != 0)
+                        {
+                            trynum++;
+                            break;
+                        }
+                        else
+                        {
+                            Map[startx][starty + 1] = maptile;
+                            starty++;
+                            isdirsel = true;
+                            break;
+                        }
+                }
+                if (isdirsel)
+                {
+                    trynum++;
+                    break;
+                }
+
+            }
+            catch
+            {
+                continue;
+            }
+        }
+        if (way == 0 && mapevent == 0 && mapshop == 0)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Debug.Log(Map[i][j]);
+                }
+                Debug.LogError("");
+            }
+        }
+        else
+        {
+            mapmake(size, way, mapevent, mapshop, startx, starty, Map);   //재귀로 구현 근데 메모리 문제 심하면 리펙토링(지금 해라)
+        }
+    }
+    public void resetpoint(int size, int way, int mapevent, int mapshop, int startx, int starty, List<List<int>> Map)
+    {
+        while (true)
+        {
+            startx = Random.Range(0, size - 1);
+            starty = Random.Range(0, size - 1);
+            bool isconnect = false;
+
+            if (Map[startx][starty] == 0)
+            {
+                if (startx - 1 > 0)
+                {
+                    if (Map[startx - 1][starty] != 0)
+                    {
+                        isconnect = true;
+                    }
+                }
+                else if (startx + 1 <= size - 1)
+                {
+                    if (Map[startx + 1][starty] != 0)
+                    {
+                        isconnect = true;
+                    }
+                }
+                else if (starty + 1 <= size - 1)
+                {
+                    if (Map[startx][starty + 1] != 0)
+                    {
+                        isconnect = true;
+                    }
+                }
+                else if (starty - 1 > 0)
+                {
+                    if (Map[startx][starty - 1] != 0)
+                    {
+                        isconnect = true;
+                    }
                 }
                 else
                 {
-                    map[i, j] = -1; // -1로 칸 초기화
+                    continue;
                 }
+                break;
             }
-        }
-        while(way <= 0)
-        {
-            int ranhor = Random.Range(0, size);
-            int ranver = Random.Range(0, size);
-
-            if(map[ranhor,ranver] == -1)
+            if (!isconnect)  //연결된 곳이 없을때
             {
-                map[ranhor, ranver] = 0;
-
-                way--;
+                continue;
             }
             else
             {
-
+                break;
             }
         }
-        Debug.Log(map[0,0]);
+        mapmake(size, way, mapevent, mapshop, startx, starty, Map);
     }
-  
 }
